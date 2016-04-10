4e8f897c9340313046afe6491c6c1c9df4ed230b
Nikola Gorgiev <ngorgiev@live.com>
2016-04-10 21:11:12 +0200

    FrontEnd Improvements

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
     <?php include("includes/header.php"); ?>
+    <?php
+    $photos = Photo::find_all();
+    ?>
     <div class="row">
     <!-- Blog Entries Column -->
     <div class="col-md-8">
-</div>
+        <?php foreach($photos as $index => $photo): ?>
+            <div class="col-xs-6 col-md-3">
+                <a class="thumbnail" href="photo.php?id=<?php echo $photo->id; ?>">
+                    <h4><?php echo $photo->title; ?></h4>
+                    <img class="home_page_photo img-responsive " src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
 
     <!-- Blog Sidebar Widgets Column -->
     <div class="col-md-4">
-
     <?php include("includes/sidebar.php"); ?>
     </div>
     <!-- /.row -->
--- photo.php
+++ photo.php
@@ -34,16 +34,17 @@ else
 $comments = Comment::find_comments_by_photo_id($photo->id);
 ?>
 
+    <div class="row">
             <div class="col-lg-8">
 
                 <!-- Blog Post -->
 
                 <!-- Title -->
-                <h1>Blog Post Title</h1>
+                <h1><?php echo $photo->title; ?></h1>
 
                 <!-- Author -->
                 <p class="lead">
-                    by <a href="#">Start Bootstrap</a>
+                    објавено од: <a href="#"> Да се смени</a>
                 </p>
 
                 <hr>
@@ -54,17 +55,13 @@ $comments = Comment::find_comments_by_photo_id($photo->id);
                 <hr>
 
                 <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="admin/<?php echo $photo->picture_path();?>" alt="">
 
                 <hr>
 
                 <!-- Post Content -->
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
-
+                <p class="lead"><?php echo $photo->caption; ?></p>
+                <p><?php echo $photo->description; ?></p>
                 <hr>
 
                 <!-- Blog Comments -->
@@ -116,3 +113,4 @@ $comments = Comment::find_comments_by_photo_id($photo->id);
 <!-- /.row -->
 
 <?php include("includes/footer.php"); ?>
+</div>
